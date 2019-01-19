cab4e6c364f2a7ae80710659e5b1f3278efdd1db
Pawel Dron <diehard@bialan.pl>
2019-01-19 10:27:13 +0100

    Exercise 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,16 @@ public class Calculator {
     public int add(int a, int b){
         return a+b;
     }
+
+    public int substraction(int a, int b){
+        return a-b;
+    }
+
+    public int multiplication(int a, int b){
+        return a*b;
+    }
+
+    public float division(int a, int b){
+        return a/b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -18,4 +18,25 @@ public class CalculatorTest {
         int result = calculator.add(10, 15);
         Assert.assertEquals(25, result);
     }
-}
+
+    @Test
+    public void substractionTest() {
+        Calculator calculator = new Calculator();
+        int result = calculator.substraction(10, 5);
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void multiplicationTest() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiplication(2, 5);
+        Assert.assertEquals(10, result);
+    }
+
+    @Test
+    public void divisionTest() {
+        Calculator calculator = new Calculator();
+        float result = calculator.division(10,3);
+        Assert.assertEquals(3.33, result, 1);
+        }
+    }
