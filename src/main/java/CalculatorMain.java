409bee1d8b235a6141e5f53e491d513ea9f81765
Pawel Dron <diehard@bialan.pl>
2019-01-19 10:05:31 +0100

    Implemented addition

---
+++ src/main/java/CalculatorMain.java
@@ -0,0 +1,9 @@
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int result =  calculator.add(4, 5);
+
+        System.out.println("result = " + result);
+    }
+}
