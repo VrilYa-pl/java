5683600bbf8778758732af1ec432525898047a6a
Pawel Dron <diehard@bialan.pl>
2019-01-19 12:45:52 +0100

    Exercise 2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,16 @@
 public class Calculator {
+
+    private void checkIfNegative(int a, int b) {
+        if (a<0 || b<0){
+            throw new NegativeNumberException();
+        }
+    }
+
     public int add(int a, int b){
+        checkIfNegative(a, b);
         return a+b;
     }
+
 
     public int substraction(int a, int b){
         return a-b;
@@ -11,7 +20,16 @@ public class Calculator {
         return a*b;
     }
 
-    public float division(int a, int b){
+    public int division(int a, int b){
+        if (b % 2 != 0){
+            throw new NotEvenNumberException();
+        }
         return a/b;
+    }
+
+    public class NegativeNumberException extends RuntimeException {
+    }
+
+    public class NotEvenNumberException extends RuntimeException {
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,42 +1,107 @@
+import javafx.beans.binding.When;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void testAddition(){
-        Calculator calculator = new Calculator();
-        int result = calculator.add(4, 3);
-        Assert.assertEquals(7, result);
+    public void testAddition() {
+
+        int a =4;
+        int b= 3;
+        int expected = 7;
+
+        int result = calculator.add(a, b);
+
+        Assert.assertEquals(expected, result);
     }
 
     @Test
     public void testAddition1() {
-        Calculator calculator = new Calculator();
         int result = calculator.add(10, 15);
         Assert.assertEquals(25, result);
     }
 
     @Test
     public void substractionTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.substraction(10, 5);
         Assert.assertEquals(5, result);
     }
 
     @Test
     public void multiplicationTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.multiplication(2, 5);
         Assert.assertEquals(10, result);
     }
 
     @Test
     public void divisionTest() {
-        Calculator calculator = new Calculator();
-        float result = calculator.division(10,3);
-        Assert.assertEquals(3.33, result, 1);
-        }
+        int a = 10;
+        int b = 5;
+        thrown.expect(Calculator.NotEvenNumberException.class);
+        int result = calculator.division(a, b);
+
+
     }
+
+    @Test(expected = ArithmeticException.class)
+    public void testArithmeticException(){
+        int a = 4;
+        int b = 0;
+
+        int result = calculator.division(a, b);
+    }
+
+    @Test
+    public void testArithmeticException2() {
+        int a = 4;
+        int b = 0;
+        int result = -1;
+
+        try{
+            result = calculator.division(a, b);
+        }
+        catch (ArithmeticException ex){
+            Assert.assertEquals("/ by zero", ex.getMessage());
+
+        }
+        Assert.assertEquals(-1, result);
+    }
+
+    @Test
+    public void testArithmeticException3() {
+        int a =4;
+        int b=0;
+        thrown.expect(ArithmeticException.class);
+        thrown.expectMessage("/ by zero");
+
+        int result = calculator.division(a,b);
+
+    }
+
+    @Test
+    public void testNotNegative() {
+        int a = 4;
+        int b =-1;
+        thrown.expect(Calculator.NegativeNumberException.class);
+
+        int result = calculator.add(a,b);
+    }
+
+
+}
